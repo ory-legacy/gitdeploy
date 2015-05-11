d9914cc89327f670e6867862a712fa5bc411214f
aeneasr <aeneas.rekkas@serlo.org>
2015-05-11 20:09:28 +0200

    flynn fixes

--- bin/flynn-install.sh
+++ bin/flynn-install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-L=/usr/local/bin/flynn && curl -sL -A \"`uname -sp`\" https://dl.flynn.io/cli | zcat >$L && chmod +x $L
-ssh-keygen -t rsa -N "" -R wv5w.flynnhub.com -f "/usr/local/.ssh/id_rsa"
+L=/app/bin/flynn && curl -sL -A \"`uname -sp`\" https://dl.flynn.io/cli | zcat >$L && chmod +x $L
+ssh-keygen -t rsa -N "" -f "/usr/local/.ssh/id_rsa"
+ssh -o StrictHostKeyChecking=no wv5w.flynnhub.com
