3ffbe9ee500af156d23e60068f65b7a5de4e39b0
aeneasr <aeneas.rekkas@serlo.org>
2015-05-11 14:01:21 +0200

    Fixed flynn-install script

--- bin/flynn-install.sh
+++ bin/flynn-install.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-L=/usr/local/bin/flynn && curl -sL -A \"`uname -sp`\" https://dl.flynn.io/cli | zcat >$L && chmod +x $L
+L=/app/bin/flynn && curl -sL -A \"`uname -sp`\" https://dl.flynn.io/cli | zcat >$L && chmod +x $L
