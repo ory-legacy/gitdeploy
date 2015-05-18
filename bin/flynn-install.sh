b3c502cdcac9997ca99e9d693b2a7bd4e4bc17a2
aeneasr <aeneas.rekkas@serlo.org>
2015-05-18 14:53:40 +0200

    flynn-install: Pointed at new cluster

--- bin/flynn-install.sh
+++ bin/flynn-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 L=/app/bin/flynn && curl -sL -A \"`uname -sp`\" https://dl.flynn.io/cli | zcat >$L && chmod +x $L
 ssh-keygen -t rsa -N "" -f "/app/.ssh/id_rsa"
-ssh -o StrictHostKeyChecking=no p3mj.flynnhub.com -p 2222
+ssh -o StrictHostKeyChecking=no 126e.flynnhub.com -p 2222
 flynn key add
--- schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS apps (
-  id      text        primary key,
-  started timestamptz NOT NULL DEFAULT now(),
-  log     text        NOT NULL DEFAULT ''
-);
